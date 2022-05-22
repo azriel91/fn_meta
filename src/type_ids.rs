899c5fcfd652f2e5cb0490104d435a319cefef2a
Azriel Hoh <azriel91@gmail.com>
2022-05-22 14:31:53 +1200

    Fix semantic typo in `type_ids.rs`.

--- src/type_ids.rs
+++ src/type_ids.rs
@@ -4,5 +4,5 @@ use smallvec::SmallVec;
 
 /// Array of [`TypeId`]s.
 ///
-/// Holds up to 8 Type IDs before overflowing onto the stack.
+/// Holds up to 8 Type IDs before overflowing onto the heap.
 pub type TypeIds = SmallVec<[TypeId; 8]>;
